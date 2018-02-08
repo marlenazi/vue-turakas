d2543bb46500603fbdc2a1ba179548f2d3f98fc3
Puukallistaja <haponen@gmail.com>
2018-02-08 03:44:17 +0100

    add lobby components

---
+++ src/views/lobby/Games.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="games height-1">
+    <ul 
+    
+    v-if="games.length > 0"
+    v-for="game in games"
+    :key="game.id">
+
+      <li 
+      class="game height-2"
+      @click="joinGame(game.id)">
+        Players: {{ game.players.length }}/{{ game.size }}
+        <div class="players"
+        v-for="player in game.players"
+        :key="player.id">
+          {{ player.name }}
+        </div>
+        <span class="join">Play</span>
+      </li>
+
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Games',
+  props: ['hero', 'games'],
+  components: {
+
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    joinGame(id) {
+      this.$socket.emit('joinGame', id, this.hero.id)
+    },   
+  },
+  computed: {
+
+  },
+  created() {
+
+  },
+  sockets: {
+    
+  }
+}
+</script>
+
+<style scoped>
+.games {
+  background: orangered;
+  flex: 1 1 auto;
+  display: flex;
+  margin: .5rem;
+  padding: .5rem;
+}
+
+.game {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: space-around;
+  align-items: center;
+  background: red;
+  height: 8rem;
+  width: 8rem;
+  margin: .5rem;
+  padding: 1rem;
+}
+.players {
+  text-align: left;
+}
+.join {
+  color: white;
+  font-size: 1.2rem;
+}
+
+</style>
---
+++ src/views/lobby/Hero.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="hero height-1">
+    <div class="avatarAndRank">
+      <img class="avatar" src="../../assets/theFool.svg" alt="" srcset="">
+      <div class="rank">
+        {{ hero.rank }}
+      </div>
+    </div>
+    <div class="name">
+      {{ hero.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Hero',
+  props: ['hero'],
+  components: {
+
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+  created() {
+
+  },
+  sockets: {
+    
+  }
+}
+</script>
+
+<style scoped>
+.hero {
+  background: orangered;
+  margin: .5rem;
+  padding: .5rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex: 1 1 auto;
+}
+.avatarAndRank {
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+}
+.avatar {
+  height: 6rem;
+  flex: 0 0 auto;
+}
+.rank {
+  border: 1px solid black;
+  width: 3rem;
+  height: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: .5rem;
+  font-size: 3rem;
+}
+.name {
+  margin: 1rem 1rem;
+  font-size: 3rem;
+}
+</style>
