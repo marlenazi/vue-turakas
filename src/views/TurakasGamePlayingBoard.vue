c71742fe89744bb5a1c5c18dfa469fcfc148ddf2
Puukallistaja <haponen@gmail.com>
2018-02-10 19:35:31 +0100

    change board card size

--- src/views/TurakasGamePlayingBoard.vue
+++ src/views/TurakasGamePlayingBoard.vue
@@ -3,6 +3,7 @@
     
     <transition-group name="fade" mode="out-in">
       <game-card
+        id="boardCard"
         v-for="card in board"
         :key="card.rank + card.suit"
         :card="card">
@@ -39,5 +40,20 @@ export default {
   border-radius: .5rem;
   border: .2rem solid $accent;
   background: $action;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+
+  min-width: 280px;
+}
+
+#boardCard {
+  height: 4rem;
+  width: 3rem;
+}
+#boardCard:nth-child(even) {
+  position: relative;
+  top: .4rem;
+  margin-left: -1rem;
 }
 </style>
