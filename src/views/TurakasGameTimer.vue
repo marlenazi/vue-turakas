715ffe2cf5ebc16f1f94220e646ed108082c14d8
Puukallistaja <haponen@gmail.com>
2018-02-17 16:23:25 +0100

    add timer component

---
+++ src/views/TurakasGameTimer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="timer">
+    <div 
+      class="time"
+      :style="{width: $_timeLength}"
+    >
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GameTimer',
+  props: {
+    time: Object,
+  },
+  computed: {
+    $_timeLength() {
+      return 100 / this.time.limit * this.time.passed + '%'
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import './../style/variables';
+
+.timer {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: .15em;
+  background: $action;
+}
+.time {
+  background: $accent;
+  height: 100%;
+}
+</style>
